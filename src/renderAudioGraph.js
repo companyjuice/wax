0c3a07313e537dd12cc65903eec60f91ab67a35b
James Wright <james@jamesswright.co.uk>
2018-10-14 17:49:23 +0100

    Adds useful TODO

--- src/renderAudioGraph.js
+++ src/renderAudioGraph.js
@@ -2,13 +2,13 @@ import connectNodes from './connectNodes';
 
 export const renderAudioGraph = (createGraphElement, context = new AudioContext()) => {
     const nodes = createGraphElement(context);
-    connectNodes(nodes);
+    connectNodes(nodes); // TODO: make connectNodes traverse a tree wrapper?
     return nodes;
 };
 
 export const renderPersistentAudioGraph = (
     createGraphElement,
-    context = new AudioContext()
+    context = new AudioContext(),
 ) => {
     let nodes = renderAudioGraph(createGraphElement, context);
 
